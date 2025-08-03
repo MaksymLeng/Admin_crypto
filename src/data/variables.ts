d8554ebddefb6fe41f69d4fe38c8c175853b5775
Maksim Vlasiuk <maksym.lang@gmail.com>
2025-08-03 19:15:52 +0200

    localhost

--- src/data/variables.ts
+++ src/data/variables.ts
@@ -1,4 +1,4 @@
 export const XP_PER_EURO = 1;
 
 export const botAPI = 'https://api.ndeposit.app';
-export const userAPI = 'https://user.ndeposit.app';
+export const userAPI = 'http://localhost:3000';
