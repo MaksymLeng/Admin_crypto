a83f42092876b2c1cf42f0f959a6b1c90f0ddfd3
Maksim Vlasiuk <maksym.lang@gmail.com>
2025-08-11 20:56:40 +0200

    change hooks

--- src/hooks/useTelegramUser.ts
+++ src/hooks/useTelegramUser.ts
@@ -1,22 +1,23 @@
 import type {TelegramUser} from "../Types/Interface.tsx";
-import {useMemo} from "react";
+import {useRef} from "react";
 
 export const useTelegramUser = (): TelegramUser | null => {
+    const startRef = useRef<string | null>(null);
     const tg = window.Telegram?.WebApp;
     const user = tg?.initDataUnsafe?.user;
 
-    const startParam = useMemo(() => {
-        const p1 = tg?.initDataUnsafe?.start_param as string | undefined;
-        const qs = new URLSearchParams(location.search);
-        const p2 = (qs.get("startapp") || qs.get("start")) ?? undefined;
-        return p1 || p2 || null;
-    }, [tg]);
-
+    if (startRef.current === null) {
+        startRef.current =
+            tg?.initDataUnsafe?.start_param ??
+            new URLSearchParams(location.search).get("startapp") ??
+            new URLSearchParams(location.search).get("start") ??
+            null;
+    }
 
     if (!user) return null;
 
     return {
         ...user,
-        startParam
+        startRef
     };
 };
